351afbedaaf4b98a34b0c27c5129a54e376152d6
Rolimm <gabrielmrolim201@gmail.com>
2024-05-28 19:18:16 -0300

    feat Schemas em moddels

--- models/receita.models.js
+++ models/receita.models.js
@@ -5,6 +5,8 @@ const { Schema } = mongoose;
 const ReceitaSchema = new Schema({
   nome: String,
   ingredientes: [String],
+  instrucoes: [String],
+  imagem: String,
 });
 
 const Receita = mongoose.model("Receita", ReceitaSchema);
